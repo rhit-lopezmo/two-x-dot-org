ae3fc34360672dfd7e1f021233d49dbdc34c95ac
rhit-lopezmo <lopezmo@rose-hulman.edu>
2026-02-06 23:24:44 -0500

    making it so only get requests work

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"net/http"
-	// "path/filepath"
-	// "two-x-dot-org/components"
+	"path/filepath"
+	"two-x-dot-org/components"
 	"two-x-dot-org/utils"
-	// "github.com/a-h/templ"
+
+	"github.com/a-h/templ"
 )
 
 var log *utils.Logger
@@ -14,32 +15,43 @@ func main() {
 	utils.InitLoggers()
 	log = utils.MainLogger
 
-	log.Println("Server is under construction...")
+	http.Handle("/", templ.Handler(components.GenHomePage()))
+	http.Handle("/code", templ.Handler(components.GenCodePage()))
+	http.Handle("/music", templ.Handler(components.GenMusicPage()))
+	http.Handle("/gallery", templ.Handler(components.GenGalleryPage()))
+	http.Handle("/blog", templ.Handler(components.GenBlogPage()))
+	http.Handle("/about", templ.Handler(components.GenAboutPage()))
 
-	// http.Handle("/", templ.Handler(components.GenHomePage()))
-	// http.Handle("/code", templ.Handler(components.GenCodePage()))
-	// http.Handle("/music", templ.Handler(components.GenMusicPage()))
-	// http.Handle("/gallery", templ.Handler(components.GenGalleryPage()))
-	// http.Handle("/blog", templ.Handler(components.GenBlogPage()))
-	// http.Handle("/about", templ.Handler(components.GenAboutPage()))
-	//
-	// staticFiles := http.FileServer(http.Dir("./static"))
-	// http.Handle("/static/", http.StripPrefix("/static/", staticFiles))
-	//
-	// // allows adjacent .js, .css, ..etc files next to their components
-	// http.Handle("/static/components/", http.StripPrefix("/static/components/",
-	// 	http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 		ext := filepath.Ext(r.URL.Path)
-	// 		if ext == ".go" || ext == ".templ" {
-	// 			http.NotFound(w, r)
-	// 			return
-	// 		}
-	// 		http.FileServer(http.Dir("./components")).ServeHTTP(w, r)
-	// 	}),
-	// ))
+	staticFiles := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", staticFiles))
+
+	// allows adjacent .js, .css, ..etc files next to their components
+	http.Handle("/static/components/", http.StripPrefix("/static/components/",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ext := filepath.Ext(r.URL.Path)
+			if ext == ".go" || ext == ".templ" {
+				http.NotFound(w, r)
+				return
+			}
+			http.FileServer(http.Dir("./components")).ServeHTTP(w, r)
+		}),
+	))
 
 	log.Println("Listening on 3000...")
-	if err := http.ListenAndServe(":3000", nil); err != nil {
+	if err := http.ListenAndServe(":3000", getOnly(http.DefaultServeMux)); err != nil {
 		log.Fatalln("ERROR: could not start server", err)
 	}
+}
+
+func getOnly(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Allow normal page loads + CORS preflight
+		if r.Method == http.MethodGet || r.Method == http.MethodOptions {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Block everything else
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	})
 }
